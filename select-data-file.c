0be2879c975beb95c48969b090c71243637ce363
Laura Neri <laura6neri@yahoo.com.br>
2024-04-12 19:34:03 -0300

    starting new file (select func)

---
+++ select-data-file.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <inttypes.h>
+#include <stdint.h>
+
+#include "data-file.h"
+
+#include "funcoes_fornecidas.h"
+#include "cabecalho-utils.h"
+#include "campo-utils.h"
