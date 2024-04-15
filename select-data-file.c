a460a646a4f0b98747c45a3e384421e474c15fab
Laura Neri <laura6neri@yahoo.com.br>
2024-04-15 18:27:34 -0300

    select function constructed based on filter function - not tested yet

--- select-data-file.c
+++ select-data-file.c
@@ -22,4 +22,52 @@ int select_data_file(const char *input_filename){
     if(!check_status(fptr)){
         return -1;
     }
+
+    int32_t reg_count = 0;
+
+    // Ler quantidade de registros existentes para parar de ler objetos removidos
+    fseek(fptr, NRO_REGARQ_OFFSET, SEEK_SET);
+    int32_t nro_reg;
+    fread(&nro_reg, 4, 1, fptr);
+
+    // Pular o cabecalho
+    fseek(fptr, HEADER_END_OFFSET, SEEK_SET);
+
+    while(reg_count < nro_reg){ // Le registros ate ler todos os registros validos
+        unsigned char rem = get_campoc(fptr);
+
+        if(feof(fptr)){
+            break;
+        }
+
+        // Registro esta removido, mover para o proximo
+        if(rem == '1'){
+            int32_t reg_size;
+            fread(&reg_size, 4, 1, fptr);
+            fseek(fptr, reg_size-5, SEEK_CUR);
+            continue;
+        }
+
+        // Pular tamanhoRegistro e Prox
+        fseek(fptr, 12, SEEK_CUR);
+
+        JOGADOR j = read_jogador_data(fptr);
+
+        print_jogador(j);
+        printf("\n");
+
+        free_jogador(&j);
+
+        reg_count++;
+    }
+
+    // Imprimir: "Registro inexistente." caso não haja nenhum registro
+    if(reg_count <= 0){
+        printf("Registro inexistente.\n\n");
+    }
+    
+    printf("\n");
+    fclose(fptr);
+    return 0;
+
 }
