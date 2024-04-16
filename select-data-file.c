bd65095a16cdddcc0ee2696081e0b7bb9311145c
Nicholas Dan <nicholaseiti@gmail.com>
2024-04-16 15:51:02 -0300

    Resolve local conflict

--- select-data-file.c
+++ select-data-file.c
@@ -11,15 +11,60 @@
 #include "campo-utils.h"
 
 int select_data_file(const char *input_filename){
-    FILE *fptr = fopen(input_filename, "rb")
-
+    // Abra o arquivo no modo leitura binaria
+    FILE *fptr = fopen(input_filename, "rb");
     if(fptr == NULL){
-        printf("Falha no processamento do arquivo.")
         return -1;
     }
 
-    // Verificação do status no cabeçalho
+    // Check de status valido
     if(!check_status(fptr)){
         return -1;
     }
+
+    int32_t reg_count = 0;
+
+    // Ler quantidade de registros existentes para parar de ler objetos removidos
+    fseek(fptr, NRO_REGARQ_OFFSET, SEEK_SET);
+    int32_t nro_reg;
+    fread(&nro_reg, 4, 1, fptr);
+
+    // Pular o cabecalho
+    fseek(fptr, HEADER_END_OFFSET, SEEK_SET);
+
+    while(reg_count < nro_reg){ // Le registros ate ler todos os registros validos
+        unsigned char rem = get_campoc(fptr);
+
+        if(feof(fptr)){
+            break;
+        }
+
+        // Registro esta removido, mover para o proximo
+        if(rem == '1'){
+            int32_t reg_size;
+            fread(&reg_size, 4, 1, fptr);
+            fseek(fptr, reg_size-5, SEEK_CUR);
+            continue;
+        }
+
+        // Pular tamanhoRegistro e Prox
+        fseek(fptr, 12, SEEK_CUR);
+
+        JOGADOR j = read_jogador_data(fptr);
+
+        print_jogador(j);
+        printf("\n");
+
+        free_jogador(&j);
+
+        reg_count++;
+    }
+
+    // Imprimir: "Registro inexistente." caso nenhum registro seja imprimido
+    if(nro_reg <= 0){
+        printf("Registro inexistente.\n\n");
+    }
+
+    fclose(fptr);
+    return 0;
 }
