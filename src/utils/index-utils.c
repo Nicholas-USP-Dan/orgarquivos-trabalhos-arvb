18db107eee97530694a73c0aa2f037d9f8e4ffd5
Nicholas Dan <nicholaseiti@gmail.com>
2024-05-15 10:00:28 -0300

    Create index-utils

---
+++ src/utils/index-utils.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "index-utils.h"
+
+#include "campo-utils.h"
---
+++ src/utils/index-utils.h
@@ -0,0 +1,8 @@
+#ifndef INDEX_UTILS_H
+#define INDEX_UTILS_H
+
+#include "../adts/min-heap.h"
+
+
+
+#endif
