61738e9f8ff5a7476553c5edb9287b25f4e53d2c
Nicholas Dan <nicholaseiti@gmail.com>
2024-05-16 13:56:26 -0300

    Remove index-utils

--- src/utils/index-utils.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-#include "index-utils.h"
-
-#include "campo-utils.h"
--- src/utils/index-utils.h
+++
@@ -1,8 +0,0 @@
-#ifndef INDEX_UTILS_H
-#define INDEX_UTILS_H
-
-#include "../adts/min-heap.h"
-
-
-
-#endif
