e769b13d7a6c6d1a8bfec178f88c69dab5150956
Nicholas Dan <nicholaseiti@gmail.com>
2024-05-16 13:56:13 -0300

    Type change and uniform use of campo-utils

--- src/filter-data-file.c
+++ src/filter-data-file.c
@@ -43,13 +43,13 @@ int filter_data_file(const char *bin_filename){
     // Leitura dos campos de busca
     JOGADOR j_query = read_query(&mask);
 
-    int32_t reg_count = 0;
-    int32_t filter_count = 0;
+    int reg_count = 0;
+    int filter_count = 0;
 
     // Ler quantidade de registros existentes
     fseek(fptr, NRO_REGARQ_OFFSET, SEEK_SET);
-    int32_t nro_reg;
-    fread(&nro_reg, 4, 1, fptr);
+    int32_t nro_reg = get_campo32(fptr);
+    // fread(&nro_reg, 4, 1, fptr);
 
     // Pular o cabecalho
     fseek(fptr, HEADER_END_OFFSET, SEEK_SET);
@@ -64,8 +64,8 @@ int filter_data_file(const char *bin_filename){
 
         // Registro esta removido, mover para o proximo
         if(rem == '1'){
-            int32_t reg_size;
-            fread(&reg_size, 4, 1, fptr);
+            int32_t reg_size = get_campo32(fptr);
+            // fread(&reg_size, 4, 1, fptr);
             fseek(fptr, reg_size-5, SEEK_CUR);
             continue;
         }
--- src/select-data-file.c
+++ src/select-data-file.c
@@ -37,12 +37,12 @@ int select_data_file(const char *bin_filename){
         return -1;
     }
 
-    int32_t reg_count = 0;
+    int reg_count = 0;
 
     // Ler quantidade de registros existentes
     fseek(fptr, NRO_REGARQ_OFFSET, SEEK_SET);
-    int32_t nro_reg;
-    fread(&nro_reg, 4, 1, fptr);
+    int32_t nro_reg = get_campo32(fptr);
+    // fread(&nro_reg, 4, 1, fptr);
 
     // Pular o cabecalho
     fseek(fptr, HEADER_END_OFFSET, SEEK_SET);
@@ -57,8 +57,8 @@ int select_data_file(const char *bin_filename){
 
         // Registro esta removido, mover para o proximo
         if(rem == '1'){
-            int32_t reg_size;
-            fread(&reg_size, 4, 1, fptr);
+            int32_t reg_size = get_campo32(fptr);
+            // fread(&reg_size, 4, 1, fptr);
             fseek(fptr, reg_size-5, SEEK_CUR);
             continue;
         }
